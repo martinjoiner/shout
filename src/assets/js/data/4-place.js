dda3a18a0753206357bf66949be90fb75d0e55b0
Martin Joiner <martin@martinjoiner.co.uk>
2016-02-12 16:13:55 +0000

    Data: Added more options to the places dictionary

--- src/assets/js/data/4-place.js
+++ src/assets/js/data/4-place.js
@@ -1,9 +1,10 @@
 
 // These are all archetypal locations 
 app.addSource( 
-	new Dictionary( 'Place', [ 	'A & E','Airport','Beach','Bus Stop','Butchers','Call centre','Canteen','Cinema','Countryside','Cruise ship','Doctors','Farm',
-								'Factory','Garden Centre','Gym','Office','Park','Swimming Pool','Fire Station','Library','Motorway Services','Night Club',
-								'Oil rig','Police Station','Power station','Restaurant','School','Showroom','Shop','Ski slope','Supermarket','Theme Park',
-								'Train Station', 'University','Zoo'
+	new Dictionary( 'Place', [ 	'A & E','Airport','Beach','Bus Stop','Butchers','Call Centre','Canteen','Cemetery','Cinema','Countryside','Cruise Ship','Doctors','Farm',
+								'Factory','Garden Centre','Gym','Office','Fire Station','Kitchen','Library','Mine','Motorway Services','Night Club','Oil Rig',
+								'Park','Petrol Station','Police Station','Power Station','Prison','Pub','Restaurant','Retirement Home',
+								'School','Scrap Yard','Showroom','Shop','Ski Slope','Supermarket','Soft Play Area','Swimming Pool', 
+								'Theme Park','Train Station','University','Zoo'
 							] )
 );
